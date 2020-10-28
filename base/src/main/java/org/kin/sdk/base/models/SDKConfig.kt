0d5f5c79803fe26a573fb627875d830e98c943ae
Kik Engineering <engineering@kik.com>
2020-10-28 13:21:04 -0400

    all: bump version to 0.3.2
    GitOrigin-RevId: 5138706b0a908f091fa291b61195f0715413a43f

--- base/src/main/java/org/kin/sdk/base/models/SDKConfig.kt
+++ base/src/main/java/org/kin/sdk/base/models/SDKConfig.kt
@@ -2,6 +2,6 @@ package org.kin.sdk.base.models
 
 object SDKConfig {
     const val platform = "JVM"
-    const val versionString = "0.3.1"
+    const val versionString = "0.3.2"
     val systemUserAgent by lazy { System.getProperty("http.agent") ?: "JVM/unspecified" }
 }
