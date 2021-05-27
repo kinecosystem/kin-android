35495aba64a6a44e05b5a7ea2dd3867082f5800d
Kik Engineering <engineering@kik.com>
2021-05-27 11:49:26 -0400

    all: version bump to 1.0.1 to fix some bugs
    GitOrigin-RevId: b040e1a190c4af85c12bd4d06cf2d5c847a9fcdd

--- base/src/main/java/org/kin/sdk/base/models/SDKConfig.kt
+++ base/src/main/java/org/kin/sdk/base/models/SDKConfig.kt
@@ -2,6 +2,6 @@ package org.kin.sdk.base.models
 
 object SDKConfig {
     const val platform = "JVM"
-    const val versionString = "1.0.0"
+    const val versionString = "1.0.1"
     val systemUserAgent by lazy { System.getProperty("http.agent") ?: "JVM/unspecified" }
 }
